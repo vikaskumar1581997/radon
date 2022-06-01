2f63bae4724dba88932a701867a99169eb822b13
vikaskumar1581997 <vikaskumar1581997@gmail.com>
2022-06-01 19:49:30 +0530

    my first edit

---
+++ src/logger/logger.js
@@ -0,0 +1,14 @@
+// const endpoint = "https://www.functionup.org"
+// const batch = "radon"
+
+// const log = function() {
+//     console.log('I am inside the log function')
+// }
+const welcome=function(){
+    console.log("i am vika skumar")
+}
+
+//module.exports.endpoint = endpoint
+module.exports.welcome=welcome
+// module.exports.batch = batch
+// module.exports.log = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const externalModule = require('./logger')
+const externalModule = require('../logger/logger.js')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
+    // console.log('The constant in logger route has a value '+externalModule.endpoint)
+    // console.log('The current batch is '+externalModule.batch)
+   // externalModule.log()
+    externalModule.welcome()
     res.send('My first ever api!')
 });
 
