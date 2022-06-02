02a4e61e65417c74cfc97910345b5e347219a3eb
vikaskumar1581997 <vikaskumar1581997@gmail.com>
2022-06-02 16:17:59 +0530

    day4 assisgnment final

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,40 +1,101 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+//const myHelper = require('../util/helper')
+//const underscore = require('underscore');
+const res = require('express/lib/response');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+
+//==============================================================================
+router.get('/movies', function (req, res) {
+   let movies =["spiderman","batman","wonderwoman"]
+    res.send(movies)
 });
-
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
+//==================================================================
+router.get('/movies/:index', function (req, res) {
+    let movies =["spiderman","batman","wonderwoman"]
     console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
+    if(req.params.index<=movies.length){
+     let a=req.params.index
+     res.send(movies[a])
+    }
+    else{
+        res.send("not a valid number")}
+ });
+
+
+
+ //========================================================================
+
+router.get('/films', function (req, res) {
+    let film = [ {
+        id : 1,
+        "name" : "lord of the rings",
+    },
+    {
+        id :2,
+       " name" : "rang de"
+    },
+       {
+           id : 3,
+           "name" : "inception"
+       },
+       {
+           id :4,
+           "name": "tenet"
+       },
+       ]
+       
+           console.log("done boss")
+        let f=[]
+       for(i=0;i<=film.length;i++){
+           let a=film[i]
+            f.push(a)
+            
+       }
+       res.send(f)
+       
+ });
+ 
+
+ //==========================================================================
+
+ router.get('/films/:filmsid', function (req, res) {
+    let film = [ {
+        id : 1,
+        "name" : "lord of the rings",
+    },
+    {
+        id :2,
+       " name" : "rang de"
+    },
+       {
+           id : 3,
+           "name" : "inception"
+       },
+       {
+           id :4,
+           "name": "tenet"
+       },
+       ]
+       //router.get('/films', function (req, res) {
+           console.log("done")
+        let f=[]
+       for(i=0;i<=film.length;i++){
+           if(req.params.filmsid==i){
+           let a=film[i]
+            f.push(a)
+            res.send(f)
+            return
+           }
+       }
+       res.send("no such movies as per number")
+       
+ });
+
+
+//=====================================================
+
 
 
 module.exports = router;
