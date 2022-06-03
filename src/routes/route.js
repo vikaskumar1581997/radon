82b341c63b288bc1c57bdbd6367019be1c69e1e9
vikaskumar1581997 <vikaskumar1581997@gmail.com>
2022-06-03 23:33:38 +0530

    prtitesh sir assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,94 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+       let r=false
+
+      let pla= req.body
+     let p =pla.name
+    for(i=0;i<players.length;i++){
+        let n=players[i]
+       let f= n.name
+        if(f==p){
+            r = true;
+           break;
+        }
+        
+
+   }
+    if (r==true){
+        res.send("not allowed")
+    }  else{
+        players.push(pla)
+       res.send( players )
+    }
+  });
+
+// router.post('/players', function (req, res) {
+    
+//     let r=false
+
+//      let pla= req.body
+//       let p =pla.name
+
+//     //let player = players.find(p => p.name == newPlayersName)
+
+//     for(i=0;i<players.length;i++){
+//         let n=players[i]
+//        let f= n.name
+//         if(f==p){
+//             r = true;
+//            break;
+//         }
+        
+
+//    }
+
+// //     //undefined is same as false/ a falsy value
+//    if (r==true) {
+// //         //Player exists
+//     res.send("This player was already added!")
+//     } else {
+//                //New entry
+//    players.push(pla)
+   
+//    res.send(players)
+//    }
+// });
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
 
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
-module.exports = router;
+module.exports = router; 
