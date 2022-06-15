c82dfbf8371d60dab071994d9405192d975a2542
vikaskumar1581997 <vikaskumar1581997@gmail.com>
2022-06-15 14:30:00 +0530

    update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,9 +10,12 @@ const mid = async function (req, res, next) {
   if (!token) return res.send({ status: false, msg: "token must be present" });
 
   console.log(token);
-  let decodeToken = jwt.verify(token, "functionup-radon");
-  console.log("ehvjehw   ",decodedToken);
-  if (decodeToken==null)  return res.send({ status: false, msg: "token is invalid" });
+  let decodedToken = //jwt.verify(token, "functionup-radon");
+  jwt.verify(token,  "functionup-radon", (err, payload) => {if(err) return res.send({msg:"Please check Token"})})
+
+
+  // console.log("ehvjehw   ",decodedToken);
+  // if (decodedToken==null)  return res.send({ status: false, msg: "token is invalid" });
     next()
 }
 module.exports.mid=mid
